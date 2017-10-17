33481da12bb0b74fc841e741ef51d27aea9c7619
Steve Thomas <steve.thomas@softwarelikeyou.com>
2017-10-17 07:25:39 -0500

    Manually add ImageView to MyUIViewController

--- HW6/thomascourtney-hw6/thomascourtney-hw6/MyViewController.swift
+++ HW6/thomascourtney-hw6/thomascourtney-hw6/MyViewController.swift
@@ -4,14 +4,26 @@ import UIKit
 class MyViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    var imageView1 = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /// Remove
+        //let imageName = "wonders1"
+        //let image = UIImage(named: imageName)
+        //imageView1 = UIImageView(image: image!)
+        imageView1.frame = CGRect(x: 20, y: 200, width: 300, height: 300)
+        imageView1.alpha = 1
+        imageView1.isHidden = false
+        //imageView1.backgroundColor = UIColor.blue
+        self.view.addSubview(imageView1)
+        
     }
     
     var wonder: String? {
         didSet {
-            imageView?.image = UIImage(named: wonder!)
+            imageView1.image = UIImage(named: wonder!)
         }
     }
 }
